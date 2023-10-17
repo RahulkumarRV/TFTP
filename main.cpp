2b0b5ce5c2b80020cbbad210c94a45b71ff1a338
RahulkumarRV <rahulkumar665550@gmail.com>
2023-10-17 14:28:27 +0530

    try lock with wait time

--- main.cpp
+++ main.cpp
@@ -3,21 +3,27 @@
 #include <mutex>
 using namespace std;
 
-mutex m;
+timed_mutex m;
 
 int count = 0;
 
-// try lock return a bool value if true then you can access to critical section other wise you continue to other part but not block
+// try lock for a given time return a bool value if true then you can access to critical section other wise you continue to other part but not block
 void print(int n, int number) {
     for (int i = 0; i < n; i++) {
         
-        if(m.try_lock()){
+        if(m.try_lock_for(chrono::seconds(2))){
+            count++;
             cout << count << " " << number <<endl;
             this_thread::sleep_for(chrono::seconds(1));
             m.unlock();
         }
     }
 }
+0 > 1, 2
+1 > 
+2 > 2, 1
+3 > 
+
 
 int main() {
     thread t(print, 2, 1);
