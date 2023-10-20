2840eca45e725e0923ed2369ba8577957e2e365c
RahulkumarRV <rahulkumar665550@gmail.com>
2023-10-20 15:55:32 +0530

    created header's for RRQ, WRQ, ERROR, ACK and DATA

--- client/client.cpp
+++ client/client.cpp
@@ -1,10 +1,121 @@
 #include <iostream>
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string>
+#include <cstring>
+#include <utility>
 
 using namespace std;
 
+pair<char*, size_t> create_RRQ_WRQ_header(uint16_t opcode, const string& filename, const string& mode) {
+    // Calculate the buffer size
+    size_t buffer_size = 2 + filename.length() + 1 + mode.length() + 1;
+    
+    // Allocate memory for the buffer
+    char* buffer = new char[buffer_size];
+    
+    // Copy data into the buffer
+    memcpy(buffer, &opcode, 2);
+    memcpy(buffer + 2, filename.c_str(), filename.length());
+    buffer[2 + filename.length()] = 0; // Null separator
+    memcpy(buffer + 2 + filename.length() + 1, mode.c_str(), mode.length());
+    buffer[buffer_size - 1] = 0; // Null terminator
+    // return the buffer containing header and it's size 
+    return make_pair(buffer, buffer_size);
+}
 
-int main(int argc, char argv[]){
+pair<char*, size_t> create_ACK_header(uint16_t opcode, uint16_t blocknumber) {
+    char* buffer = new char[4];
+    size_t buffer_size = 4;
+    memcpy(buffer, &opcode, 2);
+    memcpy(buffer + 2, &blocknumber, 2);
+
+    return make_pair(buffer, buffer_size);
+}
+
+pair<char*, size_t> create_ERROR_header(uint16_t opcode, uint16_t errorcode, string errormessage){
+    size_t buffer_size = 4 + errormessage.length() + 1;
+
+    char *buffer = new char[buffer_size];
+    memcpy(buffer, &opcode, 2);
+    memcpy(buffer + 2, &errorcode, 2);
+    memcpy(buffer + 4, errormessage.c_str(), errormessage.length());
+    buffer[buffer_size] = '\0';
+
+    return make_pair(buffer, buffer_size);
+}
+
+pair<char*, size_t> create_DATA_header(uint16_t opcode, uint16_t blocknumber, string data){
+    // 2 bytes for opcode, 2 bytes for block number, therfore that actual data should not be greater that 518 bytes in each packet
+    if(data.length() > 508){
+        cout << "data length exceeds 518 bytes";
+        return make_pair(nullptr, -1);
+    } 
+    size_t buffer_size = 4 + data.length();
+
+    char *buffer = new char[buffer_size];
+    memcpy(buffer, &opcode, 2);
+    memcpy(buffer + 2, &blocknumber, 2);
+    memcpy(buffer + 4, data.c_str(), data.length());
+    // data is null character terminated if and only if it's size is lesser thatn 518 bytes
+    if(data.length() < 508){
+        buffer[buffer_size] = '\0';
+    }
+    return make_pair(buffer, buffer_size);
+}
+
+int main() {
+    int sockfd;
+    struct sockaddr_in serverAddr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sockfd < 0) {
+        cerr << "socket error while creating socket" << endl;
+        return -1;
+    }
+
+    memset((char*)&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(9092); // TFTP default port
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    // Create an RRQ packet
+    // uint16_t opcode = htons(2); // Opcode for RRQ
+    // string filename = "example.txt";
+    // string mode = "netascii";
+    // pair<char*, size_t> header =  create_RRQ_WRQ_header(opcode, filename, mode);
+
+    // Create a ACK packet
+    // uint16_t opcode = htons(4); // Opcode for ACK
+    // uint16_t blocknumber = htons(3000); // Block number for ACK
+    // pair<char*, size_t> header = create_ACK_header(opcode, blocknumber);
+
+    // Create a error packet
+    // uint16_t opcode = htons(5);
+    // uint16_t errorcode = htons(3);
+    // string errormessage = "Unknown file";
+    // pair<char*, uint16_t> header = create_ERROR_header(opcode, errorcode, errormessage);
+
+
+    // Create a data packet
+    uint16_t opcode = htons(3);
+    uint16_t blocknumber = htons(645);
+    string data = "hey, my name is rahul, i create this client server for tftp okeokd ,ince TFTP includes no login or access control mechanisms, care must be taken in the rights granted to a TFTP server process so as not to violate the security of the server hosts file system.  TFTP is often installed with controls such that only files that have public read access are available via TFTP and writing files via TFTP is disallowed installed with controls such that only files that have public read access are available via TFTP and writing files via TFTP is disallowed";
+    uint16_t offset = 0, number_of_bytes = 0;
+
+    while(number_of_bytes < data.length()){
+        offset = number_of_bytes;
+        number_of_bytes += data.length() < 508 ? data.length() : 508;
+        cout << data.substr(offset, number_of_bytes) << endl;
+        pair<char*, uint16_t> header = create_DATA_header(opcode, blocknumber, data.substr(offset, number_of_bytes));
+        cout<<" header size : " << header.second << endl;
+        sendto(sockfd, header.first, header.second, 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+        delete[] header.first; // Clean up the buffer
+    }
+
+    
 
     return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -1,10 +1,130 @@
 #include <iostream>
-
-
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 using namespace std;
 
+// Structure to represent the RRQ packet header
+struct tftphdr {
+    short th_opcode;   // Opcode (Read Request: 1)
+    string th_stuff;   // Filename, mode, and null bytes
+};
 
-int main(int argc, char argv[]){
+void parse_RRQ_WRQ_header(char* header, uint16_t& opcode, char*& filename, char*& mode) {
+    // Extract opcode
+    memcpy(&opcode, header, 2);
+    opcode = ntohs(opcode);
+
+    // Set filename and mode pointers
+    filename = header + 2;
+    mode = filename + strlen(filename) + 1;
+}
+
+void parse_ACK_header(char* header, uint16_t &opcode, uint16_t &blocknumber){
+    memcpy(&opcode, header, 2);
+    opcode = ntohs(opcode);
+    memcpy(&blocknumber, header + 2, 2);
+    blocknumber = ntohs(blocknumber);
+}
+
+void parse_ERROR_header(char* header, uint16_t &opcode, uint16_t &errorcode, char*& errormessage){
+    memcpy(&opcode, header, sizeof(opcode));
+    opcode = ntohs(opcode);
+    memcpy(&errorcode, header + sizeof(opcode), sizeof(errorcode));
+    errorcode = ntohs(errorcode);
+    errormessage = header + sizeof(opcode) + sizeof(errorcode);
+}
+
+uint16_t parse_DATA_header(char* header, uint16_t& opcode, uint16_t& blocknumber, char*& data, size_t packetSize) {
+    // Extract opcode
+    memcpy(&opcode, header, sizeof(opcode));
+    opcode = ntohs(opcode);
+
+    // Extract block number
+    memcpy(&blocknumber, header + sizeof(opcode), sizeof(blocknumber));
+    blocknumber = ntohs(blocknumber);
+
+    // Calculate the length of the data portion based on the packet size and header length
+    size_t headerSize = sizeof(opcode) + sizeof(blocknumber);
+    size_t dataLength = packetSize - headerSize;
+
+    // Set the data pointer
+    data = header + headerSize;
+
+    return dataLength;
+}
+
+
+
+int main(){
+    
+    int sockfd;
+    struct sockaddr_in serverAddr, clientAddr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(sockfd < 0){
+        cerr<< "socket error while creating socket" <<endl;
+        return -1;
+    }
+
+    memset((char *) &serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(9092);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    int status = bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+    if(status == 0){
+        cout<< "connection established"<<endl;
+    }else{
+        return -1;
+    }
+
+    socklen_t addrLen = sizeof(clientAddr);
+
+    memset((char *) &clientAddr, 0, sizeof(clientAddr));
+    
+
+    char buffer[512]; // Adjust the buffer size accordingly
+
+    while(true){
+        // response for packet
+        int receive_status = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &addrLen);
+        
+        if(receive_status < 0){
+            cerr<< "recvfrom failed"<<endl;
+            break;
+        }
+        if(receive_status == 0){
+            cout << "connection closed"<<endl;
+            break;
+        }
+        // Extract the opcode from the received data (first 2 bytes)
+        uint16_t opcode;
+        uint16_t blocknumber;
+        char *data;
+        uint16_t data_size = parse_DATA_header(buffer, opcode, blocknumber, data, receive_status);
+        cout << "Opcode : " << opcode << endl;
+        cout << "block number : " << blocknumber << endl;
+        data[data_size] = '\0';
+        cout << "data : " << data << endl;
+        cout << "data size : " << strlen(data) << endl;
+        cout << "recived number of bytes : " << receive_status<<endl;
+        
+        if(receive_status < 512){
+            break;
+        }
+         
+    }
+
+
+
+
+
 
     return 0;
-}
+} 
